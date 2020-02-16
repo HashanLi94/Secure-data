b43b39e33a59cca49f56c71eff573865b41d2f78
Hashan Liyanage <55444962+HashanLi94@users.noreply.github.com>
2020-02-16 22:00:09 +0530

    Create CommonDetails.java

---
+++ src/com/company/CommonDetails.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public interface CommonDetails {
+    //only want this infos
+    public abstract String getFName();
+
+    public abstract String getLName();
+
+    public abstract String getDep();
+}
